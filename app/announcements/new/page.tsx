b298c27010ff0cafeff0537aeb2bf26bfdf920b3
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-11-10 17:05:19 -0500

    add simple mde editor

--- app/announcements/new/page.tsx
+++ app/announcements/new/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { Button } from "@/components/ui/button";
+
 const newAnnouncementPage = () => {
   return (
-    <div>
-      <input placeholder="hellloooooo" />
-      <button>Submit</button>
+    <div className="flex flex-col p-8 w-1/2 mx-auto space-y-3">
+      <h1>Create New Announcement: </h1>
+      <input placeholder="Header" />
+      <SimpleMDE placeholder="Add your text" />
+      <Button>Submit</Button>
     </div>
   );
 };
