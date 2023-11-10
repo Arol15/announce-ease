df4e3515a18f81272bf174662be5c7c5745f3cf4
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-11-10 17:33:45 -0500

    style new announcement form and add validations and controlled components

--- app/announcements/new/page.tsx
+++ app/announcements/new/page.tsx
@@ -3,16 +3,68 @@
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
-const newAnnouncementPage = () => {
+const formSchema = z.object({
+  header: z.string().min(2, {
+    message: "Header should not be empty.",
+  }),
+  body: z.string().min(2, {
+    message: "Body should not be empty",
+  }),
+});
+
+const NewAnnouncementPage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      header: "",
+      body: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <div className="flex flex-col p-8 w-1/2 mx-auto space-y-3">
-      <h1>Create New Announcement: </h1>
-      <input placeholder="Header" />
-      <SimpleMDE placeholder="Add your text" />
-      <Button>Submit</Button>
+      <h1>Hi, Sandy! Create your new announcement</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="header"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Header</FormLabel>
+                <FormControl>
+                  <Input placeholder="header" {...field} />
+                </FormControl>
+                <FormControl>
+                  <SimpleMDE placeholder="Add your text" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 };
 
-export default newAnnouncementPage;
+export default NewAnnouncementPage;
