c601585872193d6df74c61c8f73f6aaba711985b
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-11-09 20:43:00 -0500

    add navbar for links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <nav className="p-6 text-center border border-slate-600">
+    <nav className="p-6 flex gap-2 border border-slate-600">
       <Link href="/">Home</Link>
+      <Link href="/residents">Residents</Link>
+      <Link href="/announcements">Announcements</Link>
     </nav>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/residents">Residents</Link>
-      <Link href="/announcements">Announcements</Link>
+      Main page
     </main>
   );
 }
